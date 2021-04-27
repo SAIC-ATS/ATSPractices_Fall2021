909bb14bdcda582a21acd114ae669bc5864d874d
kmcwharter <kristinmcwharter@gmail.com>
2021-04-26 22:54:51 -0500

    url

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -18,7 +18,7 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: '/',
+  base: 'ATSPractices_Spring2021/',
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
